a096a0c7fef3f6d1460acb26835a7383cae93364
Norbert Zentai <zentai.norbert96@gmail.com>
2025-01-06 15:42:51 +0100

    Initial commit for backend

---
+++ src/test/java/hu/project/WebApp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.project.WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
