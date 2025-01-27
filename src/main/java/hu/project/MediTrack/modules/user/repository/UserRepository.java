fee4f7992ce5adf7013510c3a39bc3140b8b58af
Norbert Zentai <zentai.norbert96@gmail.com>
2025-01-27 02:35:02 +0100

    Fixed UserRepository

--- src/main/java/hu/project/MediTrack/modules/user/repository/UserRepository.java
+++ src/main/java/hu/project/MediTrack/modules/user/repository/UserRepository.java
@@ -2,9 +2,11 @@ package hu.project.MediTrack.modules.user.repository;
 
 import hu.project.MediTrack.modules.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
